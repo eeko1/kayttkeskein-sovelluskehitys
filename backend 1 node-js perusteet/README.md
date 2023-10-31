f569bff8469c2abbb750df19cfe6b01a4327b433
eeko1 <eemi.korhonen@metropolia.fi>
2023-10-31 11:47:42 +0200

    small problems fixed

--- backend 1 node-js perusteet/README.md
+++ backend 1 node-js perusteet/README.md
@@ -1,13 +1,15 @@
 # Tehtävä: Back-end 1: Node.js perusteet
 
-Node.js:n perusteista tehtävä, jossa käsitellään ohjelmointirajapinnan luomista. Tässä käytetään ns "tyhjää dataa".
+Node.js:n perusteista tehtävä, jossa käsitellään ohjelmointirajapinnan luomista. Tässä käytetään ns "tyhjää dataa". 
 
-## Käyttöönotto.
+# Käyttöönotto
 
-Kun olet ladannut tiedostot koneellesi, niin asenna node_modules tiedostot.
+Ensimmäiseksi sinun tulee ladata [node.js](https://nodejs.org/en) ja sen jälkeen ladata/kloonata tiedostot GitHubista.
+
+Kun olet ladannut tiedostot koneellesi, niin asenna vielä node_modules tiedostot terminaalia käyttäen.
 
 ```bash
-npm install
+npm install 
 ```
 
 # Serverin käynnistäminen
--- backend 1 node-js perusteet/src/index.js
+++ backend 1 node-js perusteet/src/index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import {getItems, getItemsById, postItem} from './items.js';
+import {getItems, getItemsById, postItem, putItem, deleteItem} from './items.js';
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -10,8 +10,8 @@ const server = http.createServer((req, res) => {
   // check method, url and generate response accordingly (=routing)
   if (method === 'GET' && url === '/') {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Welcome to my API</h1>');
-    res.write('<p>documentation comes here</p>');
+    res.write('<h1>Welcome to my test API</h1>');
+    res.write('<p>documentation comes here....</p>');
     res.end();
   } else if (method === 'GET' && reqParts[1] === 'items' && reqParts[2]) {
     console.log('GETting item with id', reqParts[2]);
@@ -22,7 +22,14 @@ const server = http.createServer((req, res) => {
   } else if (method === 'POST' && reqParts[1] === 'items') {
     console.log('POSTing a new item');
     postItem(req, res);
-  } else {
+  } else if (method === 'PUT' && reqParts[1] === 'items' && reqParts[2]) {
+    console.log('PUTting item with id', reqParts[2]);
+    putItem(req, res, reqParts[2])}
+    else if (method === 'DELETE' && reqParts[1] === 'items' && reqParts[2]){
+      console.log('DELETing item with id', reqParts[2]);
+      deleteItem(res, reqParts[2])
+    }
+    else {
     res.writeHead(404, {'Content-Type': 'application/json'});
     res.end('{"message": "404 Resource not found!"}');
   }
